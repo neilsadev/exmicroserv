c90671de15f5ca15b4ddef1f8c54341dc3cbd31f
neilsadev <neiloyhassan1@gmail.com>
2023-04-05 12:56:03 +0600

    payment init

---
+++ payment/main.py
@@ -0,0 +1,21 @@
+import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+# should be a different db
+redis = get_redis_connection(
+    host=os.getenv("HOST"),
+    port=os.getenv("PORT"),
+    password=os.getenv("PASSWORD"),
+    decode_responses=True
+)
