17315d2223a8f6709a95bb1ab9a31db755be982a
neilsadev <neiloyhassan1@gmail.com>
2023-04-05 12:55:08 +0600

    inv crud

--- inventory/main.py
+++ inventory/main.py
@@ -1,8 +1,17 @@
 import os
 from fastapi import FastAPI
-from redis_om import get_redis_connection
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 redis = get_redis_connection(
     host=os.getenv("HOST"),
     port=os.getenv("PORT"),
@@ -10,6 +19,36 @@ redis = get_redis_connection(
     decode_responses=True
 )
 
-@app.get("/")
-async def root():
-    return {"message": "hello world"}
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+
+    class Meta:
+        database = redis
+
+@app.get('/products')
+def all():
+    return [format(pk) for pk in Product.all_pks()]
+
+def format(pk: str):
+    product = Product.get(pk)
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity,
+    }
+
+@app.post('/products')
+def create(product: Product):
+    return product.save()
+
+@app.get('/products/{pk}')
+def get(pk: str):
+    return Product.get(pk)
+
+@app.delete('products/{pk}')
+def delete(pk: str):
+    product = Product.get(pk)
+    return Product.delete(pk)
